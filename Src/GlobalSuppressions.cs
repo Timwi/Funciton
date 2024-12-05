08c140828c66a67680776f730c75b5c289524150
Timwi <github@timwi.de>
2024-12-05 18:04:54 +0100

    Minor

--- Src/GlobalSuppressions.cs
+++ Src/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0305:Simplify collection initialization", Justification = "I prefer using x.ToArray() over [.. x]")]
 [assembly: SuppressMessage("Style", "IDE0053:Use expression body for lambda expression", Justification = "I want block bodies for lambdas with side effects", Scope = "member", Target = "~M:Funciton.FuncitonCompiler.convertToInstructions(Mono.Cecil.MethodDefinition,System.Func{Mono.Cecil.TypeReference,Mono.Cecil.Cil.VariableDefinition},System.Boolean,System.Object[])")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Let me name things the way I want")]
